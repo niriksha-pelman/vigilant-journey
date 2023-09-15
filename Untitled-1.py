c68f9591bef73d62eb0ddf176f7216d340808e95
nirikshapelman <Niriksha.Pelman@city.ac.uk>
2023-09-15 20:21:55 +0100

    Adding new windows to game

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,4 +6,12 @@ root.title("Rock, Paper, Scissors")
 
 label = tk.Label (root, text = "AI vs You" , font= ('Arial',18) )
 label.pack()
+
+def start_game():
+    new_window = tk.Toplevel()
+    new_window.geometry ("500x500")
+    tk.Label(page_1=new_window, text="This is a new window").pack()
+
+gamebutton = tk.Button (text="Start Game" , command= start_game)
+gamebutton.pack()
 root.mainloop()
