c4b491eb24e29c67dcc273bf8dd7e5b2d25c9e0f
nirikshapelman <Niriksha.Pelman@city.ac.uk>
2023-09-15 20:12:12 +0100

    Adding GUI to game

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,9 @@
-print ("niriksha pelman")
+import tkinter as tk
+root = tk.Tk()
+
+root.geometry ("500x500")
+root.title("Rock, Paper, Scissors")
+
+label = tk.Label (root, text = "AI vs You" , font= ('Arial',18) )
+label.pack()
+root.mainloop()
